7631c0040f7df1c561a254ec08dcdbcdfe4442aa
mluna1024 <mluna006@citymail.cuny.edu>
2024-11-16 17:01:22 -0500

    Change model to random forest

--- wordle_prediction.py
+++ wordle_prediction.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import pickle
 
 st.set_page_config(
     page_title="Cheatdle",
@@ -10,25 +11,16 @@ st.set_page_config(
 # Create a page header
 st.header("Wordle Score Prediction")
 
-word = st.text_input(
+with st.form("my_form"):
+    word = st.text_input(
         "Enter any five-letter combination",
         max_chars=5
     )
+    occurrence = st.number_input("Enter the word's frequency:", max_value = 0.00)
 
-base = None
-power = None
+    submitted = st.form_submit_button("Predict")
 
-freq = st.checkbox('Frequency of word known?')
-if freq:
-    a, b = st.columns(2)
-    with a:
-        base = st.number_input("Coefficient:", min_value = 0.00, max_value = 9.99)
-    with b:
-        power = st.number_input("Power of 10:", value = -3, max_value = -3)
-
-submitButton = st.button('Calculate', type="primary")
-
-if submitButton:
+if submitted:
     tweets = pd.read_csv("tweets.csv")
     words = pd.read_csv("words_freq.csv")
 
@@ -42,6 +34,9 @@ if submitButton:
     df = pd.merge(words, tweets, on='day')
     df.drop(columns=['tweet_id'], inplace=True)
 
+    filename = 'wordle_prediction.pkl'
+    model = pickle.load(open(filename, 'rb'))
+
     # For any given word:
 
     #    1. Put the word in lower case
@@ -49,37 +44,29 @@ if submitButton:
     #    3. Convert to ASCII number using ord() function
     #    4. subtract 96 to simplify char to number representation (a = 1, b = 2, c = 3, ...)
 
-    def predict_score (word, base=None, power=None):
+    def predict_score (word, occurrence):
+
+        if(not word.isalpha() or len(word) != 5):
+            raise Exception("Invalid word format. Please enter a five letter word using only alphabetic characters.")
         
         df = pd.DataFrame()
         df["word"] = [ word ]
-        df["const"] = 1.0
-        
         df["letter_1"] = df["word"].str.lower().str[0].apply(ord) - 96
         df["letter_2"] = df["word"].str.lower().str[1].apply(ord) - 96
         df["letter_3"] = df["word"].str.lower().str[2].apply(ord) - 96
         df["letter_4"] = df["word"].str.lower().str[3].apply(ord) - 96
         df["letter_5"] = df["word"].str.lower().str[4].apply(ord) - 96
-        
+
+        df["occurrence"] = [ occurrence ]
         df.drop(columns=["word"], inplace=True)
-        # Calculate weight of word frequency towards score if it is known
-        if (base != None and power != None): 
-            occurrence_weight = (16.7055 * (base * (10 ** power)))  
-        else: 
-            occurrence_weight = 0
 
-        return 4.0984   + (-0.0002 * df["letter_1"]) \
-                        + (0.0016 * df["letter_2"]) \
-                        + (-0.0023 * df["letter_3"]) \
-                        + (0.0023 * df["letter_4"]) \
-                        + (0.0009 * df["letter_5"]) + occurrence_weight
-
+        return model.predict(df)
 
     averages = df.groupby("word", as_index=False)['score'].mean()
 
     def get_scores(word, base=None, power=None):
         
-        prediction = predict_score (word, base, power) [0]
+        prediction = predict_score (word, occurrence) [0]
 
         # If word isn't found in tweet data, None is returned for the average score
         average = None
@@ -88,12 +75,12 @@ if submitButton:
 
         return prediction, average
         
-    prediction, average = get_scores(word, base, power)
+    prediction, average = get_scores(word, occurrence)
 
-    st.write(f"Word: {word}\n")
-    st.write("Predicted score via linear regression: \t{:0.2f}".format(prediction))
+    st.write(f"Word: {word}")
+    st.write("Predicted average score via random forests: \t{:0.2f}".format(prediction))
     # Print average score according to tweet data if the word exists in it
-    st.write(("No data found for this word in tweet data." if average == None else "Average score via tweet data: \t\t{:0.2f}".format(average)))
+    st.write(("No data found for this word in tweet data." if average == None else "Average score via tweet data: \t\t\t{:0.2f}".format(average)))
 
 
 
